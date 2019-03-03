e1906ac761ccee7e91190c5d441677f60a294371
Suman Ghosh <35840258+ghoshsuman@users.noreply.github.com>
2019-03-03 17:33:35 +0100

    Update Threshold.R

--- Threshold.R
+++ Threshold.R
@@ -1,5 +1,5 @@
 #This function computes Ca Imaging response of a drug application by subtracting the peak response from baseline
-#a .csv file (obtained from ImageJ), frame numbers that denote baseline and frame numbers that denote peak response are taken as input from the user
+#Inputs: .csv file (obtained from ImageJ), frame numbers for baseline, frame numbers for denote peak response
 #Only cells that meet the threshold of (baseline mean+2St.dev)and their column names are returned 
 
 #Variables - see documentation for details
@@ -34,47 +34,45 @@ for(i in 2:(ncol(A)-1)){
 r1<-as.numeric(readline("enter starting frame(row) # for baseline mean "))
 r2<-as.numeric(readline("enter ending frame(row) # for baseline mean "))
 M<-nob[r1:r2,]
-Mm<-colMeans(M)
-Mma<-array(Mm)#vector Mm is dimensionless in R, hence array of means created
+meanM<-colMeans(M)
+meanArrayM<-array(meanM)#vector 'meanM' is dimensionless in R, hence array of means 'meanArrayM' created
 
-#Subtracting baseline mean from all the values and putting into a matrix called nor(normalized)
-nor<-nob
+#Subtracting baseline mean from all the values and putting into a matrix called 'norm'
+norm<-nob
 for(i in 2:ncol(nob)){
   for(j in 1:nrow(nob)){
-   nor[j,i]<-nob[j,i]-Mma[i]
+   norm[j,i]<-nob[j,i]-meanArrayM[i]
     }
   }
 #outputfile<-readline(prompt="Enter .csv output file name ")
-#write.csv(nor,file=paste(outputfile,".csv",sep=""))
+#write.csv(norm,file=paste(outputfile,".csv",sep=""))
 
 #Finding Drug Responders
-  #Finding mean at baseline level in nor
-M1<-nor[r1:r2,]
-Mm1<-array(colMeans(M1))
-St1<-sqrt(diag(var(M1)))#formula for calculating standard dev of sample
-threshold<-array(Mm1+5*St1)
+#Finding mean at baseline level in 'norm'
+drug<-norm[r1:r2,]
+meanDrug<-array(colMeans(drug))
+sigmaDrug<-sqrt(diag(var(drug)))#formula for calculating standard dev of sample
+threshold<-array(meanDrug+5*sigmaDrug)
 
-   #Finding mean at peak level in nor
-r3<-as.numeric(readline("enter starting frame(row) # for peak mean "))
-r4<-as.numeric(readline("enter ending frame(row) # for peak mean "))
-Mp<-nor[r3:r4,]
-Mmp<-array(colMeans(Mp))
+#Finding mean at peak level in 'norm'
+r_beg<-as.numeric(readline("enter starting frame(row) # for peak mean "))
+r_end<-as.numeric(readline("enter ending frame(row) # for peak mean "))
+peak<-norm[r_beg:r_end,]
+meanPeak<-array(colMeans(peak))
 
-   #Comparing peak and threshold to extract cells that respond
+#Comparing peak and threshold to extract cells that respond
 count<-0
 percent<-0
-A_new=matrix(
-  nrow=nrow(nor),
-  ncol=ncol(nor))
-A_new[,1]=nor[,1]
-for(i in 2:ncol(nor)){
-  if(isTRUE(Mmp[i]>threshold[i])){
-   A_new[,i]=nor[,i]
+A_new=matrix(nrow=nrow(norm), ncol=ncol(norm))
+A_new[,1]=norm[,1]
+for(i in 2:ncol(norm)){
+  if(isTRUE(meanPeak[i]>threshold[i])){
+   A_new[,i]=norm[,i]
    count=count+1
    }
   }
 
-percent<-(count/(ncol(nor)-1))*100
+percent<-(count/(ncol(norm)-1))*100
 print(paste("% ONLY drug responder=",percent))
 response<-readline(prompt="Want to save .csv analysed file w/o ionomycin normalization?(y/n)")
 
@@ -85,5 +83,5 @@ write.csv(A_new,file=paste(outputfile,".csv",sep=""))
 }
 matandcolnames<-list("A_new"=A_new, "orig.cols"=orig.cols)
 assign("matandcolnames",matandcolnames,.GlobalEnv)#declaring it global so that other functions can use it
-return(matandcolnames)
+return()
 }
